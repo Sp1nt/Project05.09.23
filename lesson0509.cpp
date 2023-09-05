0f39503526f4832306864ac0b77caf771c2fac33
Spr1nt <thespr1nt0ff24@gmail.com>
2023-09-05 10:27:35 +0300

    Add more fun

--- lesson0509.cpp
+++ lesson0509.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void number(int a, int b)
+void F1()
 {
-	cout << a * b << endl;
+	cout << "F1\n";
 }
 
+void F2()
+{
+	cout << "F2\n";
+}
+
+void F3()
+{
+	cout << "F3\n";
+}
 
 int main() {
 
-	number(6, 5);
+	cout << F1 << endl << F2 << endl << F3;
 }
